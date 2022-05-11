530add86e34b7d180665a2e7c795df5e37fb2204
MinTunA20 <gin.ritani@gmail.com>
2022-05-11 11:08:15 +0700

    bai 2

---
+++ src/khaibao.java
@@ -0,0 +1,16 @@
+public class khaibao {
+    public static void main(String[] args) {
+        int i = 10;
+        double d = 20.5;
+        float f = 20.5f;
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
